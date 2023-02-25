448d9af901c8d1fabbfe5b443707f298701459be
Viacheslav009 <mstislavpushkov@gmail.com>
2023-02-25 20:45:58 +0200

    fix 1

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = creteImageCardMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+imageContainer.addEventListener('click', imageClickAction);
+function creteImageCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+function imageClickAction(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(evt.target);
+}
